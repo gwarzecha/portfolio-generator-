25a2ee464589ffdd38603d334e52f9ec101b8397
gwarz <gmwarzecha@tutanota.com>
2021-02-08 16:34:01 -0700

    created html file and modularization of js files

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
+
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+// below is an example of assignment destructuring
+const [name, github] = profileDataArgs;
 
 
-const printProfileData = profileDataArr => {
-  for (let i= 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('===============');
-
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err); 
+  
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
